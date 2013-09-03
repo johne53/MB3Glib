0be83d7706a2522fb473298d4c264c36f7383e91
John Emmas <johne53@tiscali.co.uk>
2013-09-03 09:16:43 +0100

    JOHNE - Bump version number to 2.37.8

--- win32-fixup.pl
+++ win32-fixup.pl
@@ -3,11 +3,11 @@
 
 require "../local-paths.lib";
 
-$glib_version = "2.37.6";
+$glib_version = "2.37.8";
 $major = 2;
 $minor = 37;
-$micro = 6;
-$binary_age = 3706;
+$micro = 8;
+$binary_age = 3708;
 $interface_age = 0;
 $gettext_package = "glib20";
 $current_minus_age = 0;
