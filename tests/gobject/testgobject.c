6607449f89f2070f014395d4cbf3fa471c0a0313
John Emmas <johne53@tiscali.co.uk>
2014-09-28 15:29:50 +0100

    JOHNE - Add a 'JE -' style annotation to 'tests/gobject/testgobject.c' to show that we modified some of the assertion tests
    (although the actual modifications were done some time ago)

--- tests/gobject/testgobject.c
+++ tests/gobject/testgobject.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General
  * Public License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
-
+/* Some assertion tests modified by JE - 12-04-2013 */
 #include <string.h>
 
 #undef	G_LOG_DOMAIN
