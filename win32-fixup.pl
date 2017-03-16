1fde1442f1fcf8339bedfa3762c64dde1c4edf01
John Emmas <johne53@tiscali.co.uk>
2017-03-16 18:43:20 +0000

    JOHNE - Bump the version number to 2.51.05

--- win32-fixup.pl
+++ win32-fixup.pl
@@ -3,11 +3,11 @@
 
 require "../local-paths.lib";
 
-$glib_version = "2.51.3";
+$glib_version = "2.51.5";
 $major = 2;
 $minor = 51;
-$micro = 3;
-$binary_age = 5103;
+$micro = 5;
+$binary_age = 5105;
 $interface_age = 0;
 $gettext_package = "glib20";
 $current_minus_age = 0;
